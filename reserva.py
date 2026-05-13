48695fef45dceb99d295f141a7704bee34bf7462
Jhonattan Sepúlveda <jjhonattanfloo@gmail.com>
2026-05-13 18:19:44 -0500

    commit 2 Jhonattan S

--- reserva.py
+++ reserva.py
@@ -6,3 +6,24 @@ class Reserva:
         self.servicio = servicio
         self.horas = horas
         self.estado = "Pendiente"
+from servicio import Servicio
+
+class Reserva:
+    def __init__(self, cliente, servicio, horas):
+        try:
+            if cliente == "":
+                raise ValueError("Cliente invalido")
+
+            if not isinstance(servicio, Servicio):
+                raise TypeError("Servicio invalido")
+
+            if horas <= 0:
+                raise ValueError("Horas invalidas")
+
+            self.cliente = cliente
+            self.servicio = servicio
+            self.horas = horas
+            self.estado = "Pendiente"
+
+        except Exception as e:
+            raise Exception(f"Error al crear reserva: {e}")
